bf39d48408f4826bd641cd7a0fd5348e9a9b66e3
mouad-eh <jm_elhaouari@esi.dz>
2022-12-07 22:40:44 +0100

    implemented methods & statics in schemas

--- backend/datastore/models/Category.js
+++ backend/datastore/models/Category.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
     name: String,
     color: String
-});
+}, {
+    methods: {
+        async create() {
+            return await this.save();
+        }
+    }
+}
+);
 
 const Category = mongoose.model("Category", categorySchema);
 
--- backend/datastore/models/User.js
+++ backend/datastore/models/User.js
@@ -6,7 +6,26 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     categories: [{ type: mongoose.Types.ObjectId, ref: "Category" }]
-});
+}, {
+    methods: {
+        //sign up
+        async create() {
+            return await this.save();
+        }
+    },
+    statics: {
+        //login
+        async getUser(email, password) {
+            return await this.findOne({ email, password }).exec();
+        },
+        //creating a post
+        async getAllCategories(userId) {
+            const user = await this.find({ _id: userId }).exec();
+            return user.categories;
+        }
+    }
+}
+);
 
 const User = mongoose.model("User", userSchema);
 
