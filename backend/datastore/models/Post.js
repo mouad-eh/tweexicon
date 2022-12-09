2d58b153d4e8cbd1d4f5f66ef2bad3b45f4f8f26
mouad-eh <jm_elhaouari@esi.dz>
2022-12-09 15:36:40 +0100

    refactored data schema

--- backend/datastore/models/Post.js
+++ backend/datastore/models/Post.js
@@ -1,52 +1,10 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-    userId: { type: mongoose.Types.ObjectIdj, ref: "User" },
+    userId: { type: mongoose.Types.ObjectId, ref: "User" },
     url: String,
-    categoryId: { type: mongoose.Types.ObjectId, ref: "Category" },
+    category: String,
     createdAt: { type: Date, default: Date.now, immutable: true }
-}, {
-    methods: {
-        async create() {
-            return await this.save();
-        }
-    },
-    statics: {
-        async getNextPage(userId, cursor) {
-            return await this.find(
-                {
-                    userId: userId,
-                    _id: { $gt: cursor }
-                }
-            ).limit(3).exec();
-        },
-        async getPreviousPage(userId, cursor) {
-            return await this.find(
-                {
-                    userId: userId,
-                    _id: { $lt: cursor }
-                }
-            ).limit(3).exec();
-        },
-        async getNextPageInCategory(userId, categoryId, cursor) {
-            return await this.find(
-                {
-                    userId: userId,
-                    _id: { $gt: cursor },
-                    categoryId: categoryId
-                }
-            ).limit(3).exec();
-        },
-        async getPreviousPageInCategory(userId, categoryId, cursor) {
-            return await this.find(
-                {
-                    userId: userId,
-                    _id: { $lt: cursor },
-                    categoryId: categoryId
-                }
-            ).limit(3).exec();
-        }
-    }
 });
 
 const Post = mongoose.model("Post", postSchema);
--- backend/datastore/models/User.js
+++ backend/datastore/models/User.js
@@ -5,27 +5,8 @@ const userSchema = new mongoose.Schema({
     lastName: String,
     email: String,
     password: String,
-    categories: [{ type: mongoose.Types.ObjectId, ref: "Category" }]
-}, {
-    methods: {
-        //sign up
-        async create() {
-            return await this.save();
-        }
-    },
-    statics: {
-        //login
-        async getUser(email, password) {
-            return await this.findOne({ email, password }).exec();
-        },
-        //creating a post
-        async getAllCategories(userId) {
-            const user = await this.find({ _id: userId }).exec();
-            return user.categories;
-        }
-    }
-}
-);
+    categories: [{ name: String, color: String }]
+});
 
 const User = mongoose.model("User", userSchema);
 
