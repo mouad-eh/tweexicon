2b28e6703a92f2858370e0109840984af71844bb
mouad-eh <jm_elhaouari@esi.dz>
2022-12-10 13:57:01 +0100

    changed category field property name to categoryName

--- backend/datastore/models/Post.js
+++ backend/datastore/models/Post.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
     userId: { type: mongoose.Types.ObjectId, ref: "User" },
     url: String,
-    category: String,
+    categoryName: String,
     createdAt: { type: Date, default: Date.now, immutable: true }
 });
 
 const Post = mongoose.model("Post", postSchema);
 
-module.exports = { Post };
+module.exports = { Post };  
