404b88306e32f73f67b409533d845ee6f5b9dc9b
mouad-eh <jm_elhaouari@esi.dz>
2023-01-05 22:41:52 +0100

    setting up front-end environment & testing material ui library

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { CssVarsProvider } from '@mui/joy/styles';
+import Button from '@mui/joy/Button';
+
+const App = () => {
+    return (
+        <CssVarsProvider>
+            <Button variant="solid">Hello World</Button>
+        </CssVarsProvider>
+    )
+}
+
+export default App;
