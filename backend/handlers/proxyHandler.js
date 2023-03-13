1d164f8dbcb387d33f8d44486de04e55cc5aa05d
mouad-eh <jm_elhaouari@esi.dz>
2023-03-13 20:31:09 +0100

    added encoding/decoding to the url query param

--- backend/handlers/proxyHandler.js
+++ backend/handlers/proxyHandler.js
@@ -1,10 +1,10 @@
 const request = require('request');
 
 async function proxyHandler(req, res) {
-    const { url } = req.query;
-    request(url, (error, response, body) => {
+    const decodedUrl = decodeURIComponent(req.query.url);
+    request(decodedUrl, (error, response, body) => {
         if (error || response.statusCode !== 200) {
-            return res.status(500).json({ type: 'error', message: error.message });
+            return res.status(500).json({ type: 'error', message: error });
         }
         res.send(body);
     })
