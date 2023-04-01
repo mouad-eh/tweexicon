b4a433f336256ea945b6b1e106a258018de4b9d2
mouad-eh <jm_elhaouari@esi.dz>
2023-04-01 15:58:40 +0100

    handled deleted tweets

--- frontend/src/utils/apiCalls.js
+++ frontend/src/utils/apiCalls.js
@@ -38,9 +38,23 @@ export async function fetchPostsHtml(posts) {
     const postsHtml = await Promise.all(
         posts.map(async (post) => {
             const url = encodeURIComponent(`https://publish.twitter.com/oembed?url=${post.url}&maxwidth=350&omit_script=t`);
-            const response = await axios.get(`${process.env.REACT_APP_PROXY_ENDPOINT}?url=${url}`,
-                { headers: { "Content-Type": "application/json", } }
-            )
+            try {
+                var response = await axios.get(`${process.env.REACT_APP_PROXY_ENDPOINT}?url=${url}`,
+                    { headers: { "Content-Type": "application/json", } }
+                )
+            } catch (error) {
+                //the tweet has been deleted by the author
+                //or the auther account was suspended or turned into a private account
+                return `<div style='color: #a10e25;
+                width: inherit;height: 200px;
+                display:flex;justify-content:center;align-items:center;
+                background-color: #ffe9e8;
+                padding: 0 10px;
+                margin-top: 10px;
+                border: 1px solid #a10e25;border-radius: 10px;'>
+                this tweet was deleted by the author!
+                </div>`
+            }
             return response.data.html;
         })
     )
