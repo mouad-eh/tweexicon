4e271a33a2685a89585ee312f509eecaeb617476
mouad-eh <jm_elhaouari@esi.dz>
2023-04-27 18:21:51 +0100

    improved tweets widgets render time

--- frontend/src/components/PostsGrid.js
+++ frontend/src/components/PostsGrid.js
@@ -1,20 +1,17 @@
-import { CircularProgress, Typography } from '@mui/joy';
+import { Typography } from '@mui/joy';
 import Box from '@mui/joy/Box';
 import Sheet from '@mui/joy/Sheet';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect } from 'react';
 import { getIdFromUrl } from '../utils/helperFuncs';
 
 export default function PostsGrid({ posts }) {
-    const [loading, setLoading] = useState(true);
-    const loadingRef = useRef(true);
-
     useEffect(() => {
         const parentContainer = document.getElementById('container');
         if (parentContainer) parentContainer.innerHTML = '';
         const tweetPromises = [];
         for (let post of posts) {
             const id = getIdFromUrl(post.url);
-            const tweetPromise = window.twttr.widgets.createTweet(
+            window.twttr.widgets.createTweet(
                 id,
                 parentContainer,
                 {
@@ -27,28 +24,12 @@ export default function PostsGrid({ posts }) {
                     tweet.style.marginBottom = "";
                 }
             });
-            tweetPromises.push(tweetPromise);
         }
-        Promise.all(tweetPromises)
-            .then(() => {
-                setLoading(false);
-                loadingRef.current = false;
-            })
     }, [posts])
 
 
     return (
         <Sheet sx={{ mx: "auto", position: "relative" }}>
-
-            {loading && (
-                <CircularProgress sx={{
-                    position: "fixed",
-                    top: "50%",
-                    left: "50%",
-                    marginLeft: "-20px",
-                    marginTop: "-20px"
-                }} />
-            )}
             {posts.length === 0 ?
                 <Box sx={{ textAlign: "center" }}>
                     <Typography level='body2' sx={{ fontSize: "1.5rem" }}>No posts yet !</Typography>
@@ -65,9 +46,6 @@ export default function PostsGrid({ posts }) {
                         },
                         columnWidth: "350px",
                         columnGap: "1rem",
-                        opacity: loadingRef.current ? 0 : 1,
-                        pointerEvents: loadingRef.current ? 'none' : 'auto',
-                        transition: 'opacity 0.5s ease-in-out'
                     }}>
                     </Sheet>
                 </>
